9db4ccbc79f7177491c92c346d264083586a9e0b
Nikodem <nikodem.lorenz@gmail.com>
2019-05-26 00:08:06 +0200

    PWA beginnings - added the manifest and sw

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import inputTemplates from './assets/js/templateInputer.js'
 
+
+//register a service worker
+if('serviceWorker' in navigator){
+   navigator.serviceWorker.register('sw.js')
+      .then(reg => console.log('sw registered', reg))
+      .catch(err => console.log('sw NOT registered', err))
+}
+
 const templatesAndSelectors = [
    {template: "Navbar", selector: "nav"},
    {template: "Summary", selector: ".summary"}
 ]
+inputTemplates(templatesAndSelectors) //fill the index.html with templates
 
-inputTemplates(templatesAndSelectors)
