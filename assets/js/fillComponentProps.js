ba866371fec346a31be1772f9c95eeb32bb63c58
Nikodem <nikodem.lorenz@gmail.com>
2019-05-26 22:03:14 +0200

    Included panel.css file; changed the color scheme of 'history' panels

--- assets/js/fillComponentProps.js
+++ assets/js/fillComponentProps.js
@@ -3,12 +3,15 @@ export default function(templateInfo){
    let index = 0
 
    templateInfo.props.forEach(prop => {
-      console.dir(document.querySelector(templateInfo.selector).children[index])
       const panel = document.querySelector(templateInfo.selector).children[index]
 
       panel.querySelector('header > .hour').innerHTML = prop.hour
       panel.querySelector('header > .amount').innerHTML = prop.amount
       panel.querySelector('.event').innerHTML = prop.event
+
+      if(templateInfo.selector === '.history > div') {
+         panel.className = 'history-before'
+      }
 
       index++
    })
