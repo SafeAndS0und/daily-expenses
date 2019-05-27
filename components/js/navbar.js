664f87615af70f86f89d5125e2e10a5526171534
Nikodem <nikodem.lorenz@gmail.com>
2019-05-27 13:21:13 +0200

    Created creation page and a anchor to it; fixed resp. issues

--- components/js/navbar.js
+++ components/js/navbar.js
@@ -3,10 +3,7 @@ setTimeout(() => {
    const burger = document.querySelector('.burger')
    const menu = document.querySelector('.menu')
 
-   console.log(burger)
-
    burger.addEventListener('click', () =>{
-      console.log('click')
       menu.classList.toggle('menu-active')
    })
 }, 1000) //todo yeah
