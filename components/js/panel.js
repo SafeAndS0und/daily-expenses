18e23cbb662bfb67127e79e7620d49e130a1ef51
Nikodem <nikodem.lorenz@gmail.com>
2019-05-27 12:40:53 +0200

    Dragging panels adjusted to work on mobile as well (touch events)

--- components/js/panel.js
+++ components/js/panel.js
@@ -5,13 +5,17 @@ setTimeout(() => {
 
    panels.forEach(panel => {
 
+      // pc
       let mouseX
       let difference
 
       function moveHandler(e) {
+         console.log(event)
          difference = event.x - mouseX // calculate difference to move the element
          panel.style.transform = `translateX(${difference}px)`
       }
+
+
 
       panel.addEventListener('mousedown', event => {
          mouseX = event.x
@@ -24,6 +28,30 @@ setTimeout(() => {
          panel.style.boxShadow = '0 0 0 0 rgba(0,0,0,0.75)'
          panel.style.transform = `translateX(0px)`
          panel.removeEventListener('mousemove', moveHandler)
+      })
+
+
+
+      // mobile
+      let touchX
+      let mobileDifference
+
+      function mobileMoveHandler(e) {
+         mobileDifference = event.changedTouches[0].screenX - touchX // calculate difference to move the element
+         panel.style.transform = `translateX(${mobileDifference}px)`
+      }
+
+      panel.addEventListener('touchstart', event => {
+         touchX = event.changedTouches[0].screenX
+         panel.style.boxShadow = '2px 2px 6px 0px rgba(0,0,0,0.75)'
+
+         panel.addEventListener('touchmove', mobileMoveHandler)
+      })
+
+      panel.addEventListener('touchend', () => {
+         panel.style.boxShadow = '0 0 0 0 rgba(0,0,0,0.75)'
+         panel.style.transform = `translateX(0px)`
+         panel.removeEventListener('touchmove', mobileMoveHandler)
       })
 
    })
