4f6dc76ac379c27ac1bce47a7c6b29c41aaa9436
Nikodem <nikodem.lorenz@gmail.com>
2019-06-20 20:23:38 +0200

    Modified the input handler from creation page

--- pages/creation/inputHandler.js
+++ pages/creation/inputHandler.js
@@ -2,10 +2,15 @@ document.querySelector('button').addEventListener('click', () =>{
    const inputs = document.querySelectorAll('input')
    const values = Array.from(inputs).map(input => input.value)
    const panelObj = {
-      hour: values[0],
-      amount: values[2],
-      event: values[1],
+      hour: values[2],
+      amount: values[1],
+      event: values[0],
    }
+
+   const p = document.querySelector('p')
+   p.style.opacity = '1'
+
+   setTimeout(() => p.style.opacity = '0', 1500)
 
    // adding to localstorage the data
    // creating new item or appending to the existing one
